11bb187ead3dc630fbbcffaa658e04b413504e04
Danny <Danny>
2019-06-12 17:34:07 +0100

    another mistake made with the button

--- src/main/webapp/CreateUser.js
+++ src/main/webapp/CreateUser.js
@@ -2,7 +2,7 @@ function createUser() {
     let input1 = document.getElementById("input1").value;
     let input2 = document.getElementById("input2").value;
     if (input1 !== "" && input2 !== "") {
-        let user = new user(input1, input2);
+        let user = new User(input1, input2);
         console.log(user);
         requestData(urlNeeded, "POST", user);
     }
