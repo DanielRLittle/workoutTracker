28ad7257a63c3cde4f2299e7296e002deff686ca
Danny <Danny>
2019-06-13 12:13:01 +0100

    quick change

--- src/main/webapp/GetUser.js
+++ src/main/webapp/GetUser.js
@@ -1,7 +1,7 @@
 const getUserPage = "http://35.242.137.2:8080/workoutTracker-1.0/GetUser.html"
 
 function getUser() {
-    let input = document.getElementById("input1").value;
+    let input1 = document.getElementById("input1").value;
     let input2 = document.getElementById("input2").value;
     requestData(urlNeeded, "GET", "").then((x) =>{
         let data = JSON.parse(x.responseText);
