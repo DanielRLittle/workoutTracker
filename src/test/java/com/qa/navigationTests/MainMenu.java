e84c0404c00d500c252da3a64f4ccb45d711a1cc
Hugh <hughfharding@gmail.com>
2019-06-16 19:57:54 +0100

    Added tests for navigating to and from main menu

--- src/test/java/com/qa/navigationTests/MainMenu.java
+++ src/test/java/com/qa/navigationTests/MainMenu.java
@@ -7,12 +7,14 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
+import com.qa.seleniumCRUD.CreateLandingPage;
 import com.qa.seleniumCRUD.LoginPage;
 
 public class MainMenu {
@@ -33,6 +35,7 @@ public class MainMenu {
 	}
 	
 	@Test
+	@Category(Cat1.class)
 	public void goToLogin() {
 		
 		driver.get("http://35.242.137.2:8080/workoutTracker-1.0/frontpage.html");
@@ -44,6 +47,22 @@ public class MainMenu {
 		String loginButton = loPage.loginButton();
 		
 		assertEquals("Login", loginButton);
+		
+	}
+	
+	@Test
+	@Category(Cat2.class)
+	public void goToCreateUser() {
+		
+		driver.get("http://35.242.137.2:8080/workoutTracker-1.0/frontpage.html");
+		
+		action.moveToElement(driver.findElement(By.xpath("/html/body/div[3]/button"))).click().perform();
+		driver.manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+		
+		CreateLandingPage laPage = PageFactory.initElements(driver, CreateLandingPage.class);
+		String create = laPage.createButton();
+		
+		assertEquals("Create", create);
 		
 	}
 	
